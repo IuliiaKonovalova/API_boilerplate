4cd47a08710501668c89e6191d84c3c9bd93582e
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-11-30 18:52:15 +0800

    Add postform function

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,31 @@ const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
 document.getElementById("status").addEventListener("click", e => getStatus(e));
+document.getElementById("submit").addEventListener("click", e => postForm(e));
+
+async function postForm(e) {
+
+  const form = new FormData(document.getElementById("checksform"));
+
+  const response = await fetch(API_URL, {
+    method: "POST",
+    headers: {
+      "Authorization": API_KEY,
+    },
+    body: form,
+  });
+
+  const data = await response.json();
+
+  if (response.ok) {
+    displayErrors(data);
+  } else {
+    throw new Error(data.error);
+  }
+
+}
+
+
 
 async function getStatus(e) {
   const queryString = `${API_URL}?api_key=${API_KEY}`;
