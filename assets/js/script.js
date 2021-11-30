ceb266c34cf28c34e087b0ca87ad794542307262
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-11-30 17:50:03 +0800

    Add async function to retrive aexpire day

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,15 @@
 const API_KEY = "duUmV9YEjpuIWjRu1gxOoxmX7aQ";
 const API_URL = "https://ci-jshint.herokuapp.com/api";
+
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+async function getStatus(e) {
+  const queryString = `${API_URL}?api_key=${API_KEY}`;
+  const response = await fetch(queryString);
+  const data = await response.json();
+  if (response.ok) {
+    console.log(data.expiry);
+  }
+}
