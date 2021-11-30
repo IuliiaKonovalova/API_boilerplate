ed9955db70f1cb6dd9a4923d4954401211225b29
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-11-30 19:06:19 +0800

    Add function to process options

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,9 +6,25 @@ const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal")
 document.getElementById("status").addEventListener("click", e => getStatus(e));
 document.getElementById("submit").addEventListener("click", e => postForm(e));
 
+function processOptions(form) {
+  let optArray = [];
+
+  for (let e of form.entries()) {
+    if (e[0] === "options") {
+      optArray.push(e[1]);
+    }
+  }
+
+  form.delete("options");
+
+  form.append("options", optArray.join());
+
+  return form;
+}
+
 async function postForm(e) {
 
-  const form = new FormData(document.getElementById("checksform"));
+  const form = processOptions(new FormData(document.getElementById("checksform")));
 
   const response = await fetch(API_URL, {
     method: "POST",
@@ -28,6 +44,21 @@ async function postForm(e) {
 
 }
 
+async function getStatus(e) {
+
+  const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+  const response = await fetch(queryString);
+
+  const data = await response.json();
+
+  if (response.ok) {
+    displayStatus(data);
+  } else {
+    throw new Error(data.error);
+  }
+
+}
 
 function displayErrors(data) {
 
@@ -50,25 +81,14 @@ function displayErrors(data) {
   resultsModal.show();
 }
 
-
-async function getStatus(e) {
-  const queryString = `${API_URL}?api_key=${API_KEY}`;
-  const response = await fetch(queryString);
-  const data = await response.json();
-  if (response.ok) {
-    displayStatus(data);
-  } else {
-    throw new Error(data.error)
-  }
-}
-
 function displayStatus(data) {
+
   let heading = "API Key Status";
   let results = `<div>Your key is valid until</div>`;
   results += `<div class="key-status">${data.expiry}</div>`;
 
-  document.getElementById("resultsModalTitle").innerHTML = heading;
+  document.getElementById("resultsModalTitle").innerText = heading;
   document.getElementById("results-content").innerHTML = results;
-
   resultsModal.show();
+
 }
